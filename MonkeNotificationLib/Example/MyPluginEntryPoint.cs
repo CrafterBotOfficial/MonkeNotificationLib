9d4751e36f4a7deac7a60538e30c6749b9e05f0a
Crafterbot <jackchupp@icloud.com>
2023-07-31 22:57:59 -0400

    Improved example

--- MonkeNotificationLib/Example/MyPluginEntryPoint.cs
+++ MonkeNotificationLib/Example/MyPluginEntryPoint.cs
@@ -14,18 +14,15 @@ namespace Example
         }
 
         #region Callbacks
-        public void OnPlayerEnteredRoom(Player newPlayer)
-        {
+        public void OnPlayerEnteredRoom(Player newPlayer) =>
             NotificationController.AppendMessage("Room Event", $"{newPlayer.NickName} has entered the room.");
-        }
 
-        public void OnPlayerLeftRoom(Player otherPlayer)
-        {
+        public void OnPlayerLeftRoom(Player otherPlayer)=>
             NotificationController.AppendMessage("Room Event", $"{otherPlayer.NickName} has left the room.");
-        }
+        public void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged) =>
+            NotificationController.AppendMessage("Room Event", $"The room properties have been altered, possible gamemode change attempt?".WrapColor("red"));
 
         public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps) {/*throw new System.NotImplementedException();*/}
-        public void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged) {/*throw new System.NotImplementedException();*/}
         public void OnMasterClientSwitched(Player newMasterClient) {/*throw new System.NotImplementedException();*/ }
         #endregion
     }
