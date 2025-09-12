240725cb1c10a81b13b438fe2d6c6d272d6688f4
Crafterbot <>
2025-09-12 11:26:00 -0400

    Updated example

--- Example/Plugin.cs
+++ Example/Plugin.cs
@@ -1,15 +1,34 @@
 ﻿using BepInEx;
+using MonkeNotificationLib;
+using Photon.Realtime;
 
 namespace Example;
 
-[BepInPlugin("crafterbot.notificationlib.example", "Example Notification Plugin", "1.0.1")]
+[BepInPlugin("crafterbot.notificationlib.example", "Example Notification Plugin", "1.0.2")]
 [BepInDependency("crafterbot.notificationlib")]
 public class Plugin : BaseUnityPlugin
 {
-    private Callbacks networkCallbacks;
-
-    private void Start()
+    private void Awake()
     {
-        networkCallbacks = new Callbacks();
+        GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+    }
+
+    private void OnGameInitialized()
+    {
+        Logger.LogDebug("Game initialized");
+        if (NetworkSystem.Instance is not NetworkSystem networkSystem)
+            return;
+        networkSystem.OnPlayerJoined += OnPlayerEnteredRoom;
+        networkSystem.OnPlayerLeft += OnPlayerLeftRoom;
+    }
+
+    public void OnPlayerEnteredRoom(NetPlayer newPlayer)
+    {
+        NotificationController.AppendMessage("Room Event", $"{newPlayer.SanitizedNickName} has entered the room.");
+    }
+
+    public void OnPlayerLeftRoom(NetPlayer otherPlayer)
+    {
+        NotificationController.AppendMessage("Room Event", $"{otherPlayer.SanitizedNickName} has left the room.");
     }
 }
